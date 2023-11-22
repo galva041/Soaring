7476b0e90e76e45c5d8d70744a8f96aa8d456a8e
Gabriela Alvarez <sofiaalvarez@Adrianas-MacBook-Air.local>
2023-11-22 01:14:23 -0800

    required text fields

--- components/txtAnsPopUp.js
+++ components/txtAnsPopUp.js
@@ -1,35 +1,37 @@
 import { useState } from "react";
 import { useRouter } from 'next/router';
 
-const TxtAnsPopUp = ({currPage}) => {
+const TxtAnsPopUp = () => {
     const router = useRouter();
 
     const handleClick = () => {
-        if (currPage === "Connie02") {
-            router.push('/connie03');
-        } else if (currPage === "Connie04") {
-            router.push('/badge');
-        }
-        
+        router.push('/connie03');    
     };
 
     var [affAns, setAffAns] = useState(false);
-    var [fourAns, setFourAns] = useState(false);
+    const [textareaValue, setTextareaValue] = useState('');
 
-    const handlePopUp = () => {
-
-        
-        if (currPage === 'Connie02') {
-            setAffAns(true);
-        } else if (currPage === 'Connie04') {
-            setFourAns(true);
-        }
+    const handleCheckClick = () => {
+        setAffAns(true);
+        // console.log('Checking textarea:', textareaValue);
     };
     
     return (
-        <div className="w-screen px-6 ">
+        <div className="w-screen px-6"> 
+            <textarea rows="4" cols="50" type="text" 
+                placeholder="For example, Connie can say “I’m intelligent.” Now you try! Tap here to type."
+                className="mt-3 mb-10"
+                value={textareaValue}
+                onChange={(e) => setTextareaValue(e.target.value)}
+                required>
+            </textarea>
+
             <div className='grid grid-flow-row gap-3 w-full mt-1.5 z-0 relative'>
-                <button className='btn-blue' onClick={handlePopUp}>Check</button>
+                <button className='btn-blue' onClick={handleCheckClick}
+                    disabled={!textareaValue.trim()}
+                >
+                    Check
+                </button>
             </div>
 
         {affAns? 
@@ -46,22 +48,6 @@ const TxtAnsPopUp = ({currPage}) => {
                     <button className="btn-blue" onClick={handleClick}>Next</button>
                 </div>
             </div>
-
-        : fourAns? 
-        <div className="h-full w-screen flex flex-col justify-end items-end z-3 absolute top-0 left-0 bg-black bg-opacity-25">
-        <div className="flex flex-col justify-around items-start w-full h-1/4 rounded-t-3xl bg-white p-8">
-                    <h3>
-                        ✅ &nbsp;&nbsp; Fantastic Job!
-                    </h3>
-
-                    <h2 className="mb-3">
-                        Those are great affirmations!
-                    </h2>
-
-                    <button className="btn-blue" onClick={handleClick}>Finish</button>
-                </div>
-            </div>
-
         : null}
         </div>
     )
--- pages/connie04/index.js
+++ pages/connie04/index.js
@@ -1,17 +1,13 @@
-import { useRouter } from 'next/router';
 import Image from 'next/image'
 import c04 from '../../public/connie/Connie04.png'
 import StatusBar from '@/components/statusBar'
-import TxtAnsPopUp from '@/components/txtAnsPopUp';
 import Affirmation from '@/components/affirmation';
 
 
 export default function Connie04() {
 
   return (
-    <main
-      className=""
-    >
+    <main>
       <StatusBar fraction='4/4' finish="0"/>
 
       <div className='flex flex-col w-full'>
@@ -21,7 +17,6 @@ export default function Connie04() {
       </div>
 
       <div className='h-min w-[65%] sm:w-[70%] md:w-[55%] lg:w-[40%] xl:w-[30%]'>
-      {/* <div className='h-min w-[40%]'> */}
         <Image
           src={c04}
           alt="Connie04"
@@ -29,12 +24,7 @@ export default function Connie04() {
         />
       </div>
 
-        <Affirmation num="1"/>
-        <Affirmation num="2"/>
-        <Affirmation num="3"/>
-
-        <TxtAnsPopUp currPage="Connie04"/>
-
+      <Affirmation/>
     </main>
   )
 }

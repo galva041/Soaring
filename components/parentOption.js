6ef73b32dc59fe81b0f6d49d26a77d264c7d9212
Gabriela Alvarez <gabrielaalvarez@Gabrielas-MacBook-Air.local>
2024-03-28 02:07:19 -0700

    parent home styled

---
+++ components/parentOption.js
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router'
+import Image from 'next/image';
+
+import listening from '@/public/parent/listeningCorner.png'
+import reading from '@/public/parent/readingCorner.png'
+import askother from '@/public/parent/askOtherParents.png'
+import trending from '@/public/parent/trendingTopics.png'
+import other from '@/public/parent/otherResources.png'
+
+const ParentOption = ({title}) => {
+    const router = useRouter();
+
+    const navToParentHome = () => {
+        router.push('/parent/parentHome');
+    };
+
+    const navToParentInsights = () => {
+        router.push('/parent/parentInsights');
+    };
+    
+    return (
+        <div className='flex-col space-y-3'>
+          <h6>{title}</h6>
+          <Image
+            alt="listening corner"
+            src={title == "Listening Corner"? listening: title == "Reading Corner"? reading : title == "Ask Other Parents"? askother : title == "Trending Topics"? trending: other} 
+            width='100%'
+            height='100%'
+            className="object-cover h-[144px] w-[344px] self-center rounded-md"
+          /> 
+          
+        </div> 
+    )
+};
+
+export default ParentOption;
--- pages/parent/parentHome.js
+++ pages/parent/parentHome.js
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+
 import { IoChevronBackOutline } from "react-icons/io5";
 import { RiHome2Line, RiSearchLine } from "react-icons/ri";
+import { FaCircle } from "react-icons/fa";
 
 import ParentNavbar from '@/components/parentNavbar';
+import ParentOption from '@/components/parentOption';
 
 
 export default function ParentHome() {
@@ -20,7 +23,7 @@ export default function ParentHome() {
 
   return (
     <main
-      className="relative bg-gray-blue h-screen pt-4"
+      className="relative bg-gray-blue w-screen h-max pt-4"
     >
         <div className='z-10 flex w-screen pl-6 pr-10'>
             <button className="btn-nav" onClick={() => router.push('/childorparent')}>
@@ -32,9 +35,63 @@ export default function ParentHome() {
             </button>
         </div>
 
-        <div className='flex items-center justify-around space-x-14 w-screen'>
+        <div className='flex items-center justify-around space-x-14 w-screen mt-3 mb-5'>
             <h5>Hey there! 👋</h5> 
             <RiSearchLine size={28} color='#313131'/>
+        </div>
+
+        <div className='flex-col justify-center item-center space-y-5 mb-14'>
+          <ParentOption title="Listening Corner" />
+          <ParentOption title="Reading Corner"/>
+          <ParentOption title="Ask Other Parents"/> 
+
+          <div className='space-y-3'>
+            <h6>Ask the Experts</h6>
+
+            <div className='overflow-x-auto flex space-x-4 w-[344px]'>
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Bilkis</h4>
+              </div>
+
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Dr. Diane</h4>
+              </div>
+
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Chris</h4>
+              </div>
+
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Dr. Paul</h4>
+              </div>
+
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Dr. Zara</h4>
+              </div>
+
+              <div className='flex-col justify-center items-center'>
+                <FaCircle size={72} color='#777777'/>
+                <h4 className='text-center pt-2'>Hannah</h4>
+              </div>
+              
+            </div>
+
+            <button className='btn-blue'>
+              Ask the Experts
+            </button>
+          </div>
+
+
+          <ParentOption title="Trending Topics"/>
+
+          <div className="border-t border-1.5 w-[344px] border-outline-gray "></div>
+
+          <ParentOption title="Other Resources"/>
         </div>
 
         <ParentNavbar home={true} insights={false}/>
